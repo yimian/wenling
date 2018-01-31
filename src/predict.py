51f6bd0bf2fbbf8532191fe7316cd6b1698edc3d
ruoningwen <ruoningwen@gmail.com>
2018-01-31 11:54:20 +0800

    finish predictor test

--- src/predict.py
+++ src/predict.py
@@ -2,7 +2,6 @@
 import csv
 import re
 import jieba
-import numpy as np
 import pickle
 import keras
 from keras.preprocessing import sequence
@@ -45,11 +44,9 @@ def index_to_label(index):
 
 if __name__ == '__main__':
     o_predictor = Predictor()
-
     label_list = ['5', '3', '1']
     x = []
     y_label = []
-
     with open('data/tuofa_comments.csv') as f:
         f_csv = csv.reader(f)
         headers = next(f_csv)
@@ -58,19 +55,18 @@ if __name__ == '__main__':
             rate = re.findall(r'\d+\.?\d*', row[2])[0]
             x.append(text.replace('脱发', '掉发'))
             y_label.append(rate)
+    predictor_dict = init_predictor_dict()
+    for name in predictor_dict:
+        print(name)
+        predictor = predictor_dict[name]
+        y_predict = predictor.predict(x)
 
-    y_predict = o_predictor.predict(x).tolist()
+        y_predict_label = list(
+            map(lambda x: label_list[x.index(max(x))], y_predict))
 
-    for i in range(30):
-        print(x[i], y_predict[i])
-
-    y_predict_label = list(
-        map(lambda x: label_list[x.index(max(x))], y_predict))
-
-    error_count = 0
-    for i in range(len(y_label)):
-        if y_predict_label[i] != y_label[i]:
-            error_count += 1
-
-    print('Accuracy:')
-    print((len(y_label) - error_count) / len(y_label))
+        error_count = 0
+        for i in range(len(y_label)):
+            if y_predict_label[i] != y_label[i]:
+                error_count += 1
+        print('Accuracy:')
+        print((len(y_label) - error_count) / len(y_label))
